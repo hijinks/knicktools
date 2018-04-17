d7cd9a334a3eb3fd91f0b1d472a6c9c3f39dad34
Sam Brooke <sam@kyru.co.uk>
2018-04-17 14:08:30 +0100

    Correct library paths

--- kt_start.m
+++ kt_start.m
@@ -1,8 +1,8 @@
-addpath('./topotoolbox')
-addpath('./topotoolbox/tools_and_more/')
-addpath('./topotoolbox/topoapp/')
+addpath('./lib/topotoolbox')
+addpath('./lib/topotoolbox/tools_and_more/')
+addpath('./lib/topotoolbox/topoapp/')
 close all;
 clear;
-disp('Welcome to KnickTools v0.3');
+disp('Welcome to KnickTools v0.4');
 
 knicktools;
